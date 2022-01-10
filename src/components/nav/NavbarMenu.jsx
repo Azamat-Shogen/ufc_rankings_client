74f3b68e2635c4f9d7bfb8be6fbcb66140ba3175
Azamat Shogen <baki47hanma@gmail.com>
2022-01-09 22:45:45 -0600

    api update

--- src/components/nav/NavbarMenu.jsx
+++ src/components/nav/NavbarMenu.jsx
@@ -6,31 +6,73 @@ import {
     NavbarToggler,
     NavItem,
     NavLink,
+    Collapse,
+    Nav
 } from "reactstrap";
 
 
 
 const NavbarMenu = () => {
 
-
     return (
-        <div>
-         <Navbar
-             color="dark"
-             dark
-             expand="md"
-         >
-             <NavbarBrand className="m-2" href="/">
-                 <img src={ufc_logo} style={{height: "40px", width: "70px", margin: "auto"}} alt=""/>
-             </NavbarBrand>
+        <nav className="navbar navbar-expand-md bg-dark navbar-dark p-3">
+            <div className="col-lg-4">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul className="navbar-nav container justify-content-around">
+                    <li className="nav-item">
+                        <a className="nav-link" href="home">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Rankings</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Athletes</a>
+                    </li>
+                </ul>
 
-             <NavbarText>
-               UFC RANKINGS
-             </NavbarText>
+            </div>
+            </div>
 
-         </Navbar>
-        </div>
+            <div className="col-lg-4">
+
+                <img src={ufc_logo} style={{height: "40px", width: "70px", margin: "auto"}} alt=""/>
+
+            </div>
+            <div className="col-lg-4"><a className="nav-link" style={{float: "right"}} href="#">AZ</a></div>
+
+        </nav>
     )
 }
 
 export default NavbarMenu
+
+
+// <Navbar color="dark" dark expand="md">
+{/*<NavbarBrand className="m-2" href="/">*/}
+{/*    <img src={ufc_logo} style={{height: "40px", width: "70px", margin: "auto"}} alt=""/>*/}
+{/*</NavbarBrand>*/}
+
+{/*<NavbarText>*/}
+{/*  UFC RANKINGS*/}
+{/*</NavbarText>*/}
+// </Navbar>
+
+// <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+//     <ul className="navbar-nav">
+//         <li className="nav-item">
+//             <a className="nav-link" href="/">Home</a>
+//         </li>
+//         <li className="nav-item">
+//             <a className="nav-link" href="/about">About</a>
+//         </li>
+//         <li className="nav-item">
+//             <a className="nav-link" href="/user">Users</a>
+//         </li>
+//
+//     </ul>
+//
+//     <img src={ufc_logo} style={{height: "40px", width: "70px", margin: "auto"}} alt=""/>
+// </nav>
