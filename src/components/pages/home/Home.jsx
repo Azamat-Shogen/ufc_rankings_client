3b9ddfab11a5d6110bb062823e7e7f775315f850
Azamat Shogen <baki47hanma@gmail.com>
2022-01-12 16:40:03 -0600

    error work

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <Container className="bg-white border mt-5 mb-5">
            <h1>Work still in progress... 🥱</h1>
-            <Spinner />
+            {/*<Spinner />*/}
         </Container>
     )
 }
