b61e6ebe9bc4d14cde6609242ed7bd9de771a0a2
Azamat Shogen <baki47hanma@gmail.com>
2022-01-12 18:51:16 -0600

    req

--- src/api/requests.js
+++ src/api/requests.js
@@ -3,9 +3,10 @@ import axios from "axios";
 export const fetchWeightClassesData = async () => {
     try{
         // TODO: for deployment use
-        const data = await axios.get(process.env.REACT_APP_FEED_WEIGHTCLASSES)
+       // const data = await axios.get(process.env.REACT_APP_FEED_WEIGHTCLASSES)
             // TODO: for local use
             //const data = await axios.get(process.env.REACT_APP_API_WEIGHTCLASSES)
+            const data = await axios.get("http://ec2-18-225-28-103.us-east-2.compute.amazonaws.com:8000/api/weightclasses")
             .then(res => res.data)
         return data
     }catch (err){
@@ -17,9 +18,10 @@ export const fetchWeightClassesData = async () => {
 export const fetchRankingsAthletesData = async () => {
     try{
         // TODO: for deployment use
-        const data = await axios.get(process.env.REACT_APP_FEED_ATHLETES_RANKINGS)
+        //const data = await axios.get(process.env.REACT_APP_FEED_ATHLETES_RANKINGS)
             // TODO: for local use
             //const data = await axios.get(process.env.REACT_APP_API_ATHLETES_RANKINGS)
+            const data = await axios.get("http://ec2-18-225-28-103.us-east-2.compute.amazonaws.com:8000/api/rankings-athletes")
             .then(res => res.data)
         return data
     }catch (err){
@@ -31,9 +33,10 @@ export const fetchRankingsAthletesData = async () => {
 export const fetchAthletesData = async (page) => {
     try{
         // TODO: for deployment use
-        const data = await axios.get(`${process.env.REACT_APP_FEED_ATHLETES_ALL}/${page}`)
+        //const data = await axios.get(`${process.env.REACT_APP_FEED_ATHLETES_ALL}/${page}`)
             // TODO: for local use
            // const data = await axios.get(`${process.env.REACT_APP_FIGHTERS_ALL}/${page}`)
+            const data = await axios.get(`http://ec2-18-225-28-103.us-east-2.compute.amazonaws.com:8000/api/fighters/${page}`)
             .then(res => res.data)
         return data
 
