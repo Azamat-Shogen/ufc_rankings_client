726c7eff78ad8a55343dc249f35750bf7c54f054
Azamat Shogen <baki47hanma@gmail.com>
2022-01-16 16:05:31 -0600

    home page slideshow

--- src/components/pages/fighters/Athletes.jsx
+++ src/components/pages/fighters/Athletes.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
-import {fetchAthletesData} from "../../../api/requests";
-import {Row, Spinner, PaginationItem, PaginationLink, Pagination} from 'reactstrap';
+import {fetchAthletesData, fetchMenData, fetchWomenData} from "../../../api/requests";
+import {Row, Spinner, PaginationItem, PaginationLink, Pagination, Nav} from 'reactstrap';
 import {createRows2} from "../../../utils";
 import AthleteCard from "./AthleteCard";
 import withRouter from "../../../withRouter";
@@ -8,20 +8,30 @@ import "./Athletes.css"
 import pageContainer from "../../../pageContainer";
 
 
+const pageNumbners = [1, 2, 3]
+const dataToLoad = [{fightes:"ALL", selected: true}, {fightes:"MEN", selected: false}, {fightes:"WOMEN", selected: false}]
 
 const Athletes = (props) => {
 
    // const path = props.router.location.pathname
-    const pageNumbners = [1, 2, 3]
+    
 
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [rows, setRows] = useState([])
+    const [filteredData, setFilteredData] = useState(dataToLoad)
 
 
     useEffect( () => {
         setLoading(true);
-       const data = fetchAthletesData(page)
+        let data;
+        const selectedData = filteredData.find(el => el.selected === true).fightes
+
+        console.log(selectedData)
+        if(selectedData === "ALL") data = fetchAthletesData(page)
+        else if(selectedData === "MEN") data = fetchMenData(page)
+        else data = fetchWomenData(page)
+
         data.then( el => {
             setLoading(false);
             setRows(createRows2(el))
@@ -29,7 +39,22 @@ const Athletes = (props) => {
         return () => {
            console.log("unmount athletes")
         }
-    }, [page])
+    }, [page, filteredData])
+
+    
+
+    const toggleFilter = (filterByData) => {
+      const temp = [...filteredData]
+      temp.map(el => {
+          if(el.fightes === filterByData){console.log("found"); el.selected = true}
+          else { el.selected = false}
+          return el
+      })
+
+      console.log(temp)
+      setPage(1)
+      setFilteredData(temp)
+    }
 
 
     const handleNextClick = () => {
@@ -50,10 +75,17 @@ const Athletes = (props) => {
         <>
             <h1>ATHLETES</h1>
            <div className="athletes-toggle">
-               <div className="data-to-load"><h5>ALL</h5></div>
-               <div className="data-to-load"><h5>MEN</h5></div>
-               <div className="data-to-load"><h5>WOMEN</h5></div>
+               <div className="data-to-load" onClick={() => toggleFilter("ALL")}>
+               <h5 className={filteredData[0].selected ? "fighters-selected": "fighters-not-selected"}>ALL</h5>
+               </div>
+               <div className="data-to-load" onClick={() => toggleFilter("MEN")}>
+               <h5 className={filteredData[1].selected ? "fighters-selected": "fighters-not-selected"}>MEN</h5>
+               </div>
+               <div className="data-to-load" onClick={() => toggleFilter("WOMEN")}>
+               <h5 className={filteredData[2].selected ? "fighters-selected": "fighters-not-selected"}>WOMEN</h5>
+               </div>
            </div>
+            
             <div>
             {
                 rows.map((row, i) => <Row md="4" sm="2" xs="1" className="mt-4 mb-4" key={i}>
@@ -74,6 +106,9 @@ const Athletes = (props) => {
                 </Spinner></div> : renderRows()
 
             }
+
+            {
+                !loading && 
             <div className="d-flex flex-row-reverse" >
             <Pagination aria-label="Page" size="sm">
             
@@ -122,7 +157,7 @@ const Athletes = (props) => {
                 </PaginationItem>
                 }
                 
-                <PaginationItem>
+                <PaginationItem disabled={rows.length < 3}>
                     <PaginationLink
                         // href="/"
                         onClick={handleNextClick}
@@ -137,7 +172,7 @@ const Athletes = (props) => {
                     />
                 </PaginationItem>
             </Pagination>
-            </div>
+            </div> }
             </React.Fragment>
     )
 }
